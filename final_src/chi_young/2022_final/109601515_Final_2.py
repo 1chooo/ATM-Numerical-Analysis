cb57e20b8f67b50855e7af6060be824eaf59f286
1chooo <hugo970217@gmail.com>
2023-01-04 00:28:01 +0800

    add: final src

---
+++ final_src/chi_young/2022_final/109601515_Final_2.py
@@ -0,0 +1,23 @@
+from scipy import integrate
+from scipy.special import erf
+import numpy as np
+def q(z):
+    qz = -6.01 * (z**8) + 4.92 * (z**7) - 0.024 * (z**4) + 0.13 * (z**3) - 0.37 * (z**2) - 1.88 * z + 20.00
+    return qz
+Pw = 1000
+g = 9.8
+para = 1 / (Pw * g)
+
+x = np.array([0.,1000.,2000.,3000., 4000., 5000., 6000., 7000., 8000., 9000., 10000.])
+fx = q(x)
+trap = np.trapz(fx, dx=1)
+simp = integrate.simps(fx,dx=1000)
+romb = integrate.romberg(q, 0, 10000)
+quad = float((integrate.quadrature(q, 0.0, 10000.0))[0])
+
+print('2.')
+print('(a):')
+print('Trapezoid:', para * trap, '\nSimpson 1/3:', para * simp, '\nRomberg:', para * romb)
+print('----------------------------------------------')
+print('(b):')
+print('Gauss-quadrature:', para * quad)
